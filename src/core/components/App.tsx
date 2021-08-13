86708bb32d0dec16ba5422faefa8c5df45531775
Seb <enitoni@gmail.com>
2021-08-13 22:09:35 +0200

    add border-box sizing and global font color

--- src/core/components/App.tsx
+++ src/core/components/App.tsx
@@ -1,14 +1,19 @@
 import React from "react"
 
-import { Global, css, ThemeProvider } from "@emotion/react"
+import { Global, css, ThemeProvider, Theme } from "@emotion/react"
 import { lightTheme } from "../theming"
 
 import { Screen } from "../components/Screen"
 
-const globalStyle = css`
+const globalStyle = (theme: Theme) => css`
+  * {
+    box-sizing: border-box;
+  }
+
   body {
     margin: 0;
     font-family: sans-serif;
+    color: ${theme.fontColor.normal};
   }
 `
 
