cc89942e13de48e315e0feb2c025e65b32ae7c10
Seb <enitoni@gmail.com>
2021-08-13 23:42:31 +0200

    Add UpcomingView component

--- src/schedule/components/UpcomingView.tsx
+++ src/schedule/components/UpcomingView.tsx
@@ -2,14 +2,40 @@ import styled from "@emotion/styled"
 import React from "react"
 import { Card } from "../../core/components/Card"
 import { store } from "../../core/store"
+import { ScheduleItemSummary } from "./ScheduleItemSummary"
 
 const Container = styled.div`
   display: flex;
+  flex-direction: column;
+
+  height: 100%;
 `
 
-const Content = styled.div``
+const Body = styled.div`
+  display: flex;
+  flex: 1;
+`
+
+const Footer = styled.div`
+  text-align: center;
+
+  font-weight: 600;
+  font-size: 16px;
+
+  color: ${(props) => props.theme.fontColor.muted};
+`
+
+const Content = styled.div`
+  max-width: 590px;
+  flex: 1;
+`
 
 const NextCard = styled(Card)`
+  margin-top: 24px;
+  margin-bottom: 42px;
+`
+
+const LaterListCard = styled(Card)`
   margin-top: 24px;
 `
 
@@ -18,10 +44,24 @@ export function UpcomingView() {
 
   return (
     <Container>
-      <Content>
-        <h1>Neste program</h1>
-        <NextCard>...</NextCard>
-      </Content>
+      <Body>
+        <Content>
+          <h1>Neste program</h1>
+          <NextCard>
+            <ScheduleItemSummary item={next} />
+          </NextCard>
+          <h1>Senere</h1>
+          <LaterListCard>
+            {scheduleItems.slice(0, 3).map((item) => (
+              <ScheduleItemSummary key={item.id} item={item} />
+            ))}
+          </LaterListCard>
+        </Content>
+      </Body>
+      <Footer>
+        Alt innhold sendes på medlemmers eget ansvar. Se frikanalen.no for
+        kontakt- og redaktørinformasjon.
+      </Footer>
     </Container>
   )
 }
