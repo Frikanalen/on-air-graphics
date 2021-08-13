aa7ee67a384e4fdebd65151248b8dfea3d80e1a2
Seb <enitoni@gmail.com>
2021-08-13 22:32:31 +0200

    add custom font and h1 syling

--- src/core/components/App.tsx
+++ src/core/components/App.tsx
@@ -6,14 +6,22 @@ import { lightTheme } from "../theming"
 import { Screen } from "../components/Screen"
 
 const globalStyle = (theme: Theme) => css`
+  @import url("https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@400;700;900&display=swap");
+
   * {
     box-sizing: border-box;
   }
 
   body {
     margin: 0;
-    font-family: sans-serif;
+    font-family: "Source Sans Pro", sans-serif;
     color: ${theme.fontColor.normal};
+  }
+
+  h1 {
+    font-size: 48px;
+    font-weight: 900;
+    margin: 0px;
   }
 `
 
