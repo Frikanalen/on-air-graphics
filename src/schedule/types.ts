fe2ad1dc5dd9435f2b955200e9983f76263ce2fc
Seb <enitoni@gmail.com>
2021-08-13 23:28:11 +0200

    add Organization data to ScheduleItem type

--- src/schedule/types.ts
+++ src/schedule/types.ts
@@ -1,9 +1,16 @@
-export type Video = {
+export type Organization = {
   id: number
   name: string
 }
 
+export type Video = {
+  id: number
+  name: string
+  organization: Organization
+}
+
 export type ScheduleItem = {
+  id: number
   video: Video
   starttime: string
   endtime: string
