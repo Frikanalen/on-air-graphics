c0dba8b6dbcb07306b5138fcfa1b3119b0cf08c5
Seb <enitoni@gmail.com>
2021-08-14 13:37:34 +0200

    Tweak humanizeDate to be more granular

--- src/schedule/helpers/humanizeDate.ts
+++ src/schedule/helpers/humanizeDate.ts
@@ -1,5 +1,6 @@
 import {
   differenceInHours,
+  differenceInMinutes,
   differenceInSeconds,
   format,
   formatDistance,
@@ -9,18 +10,18 @@ import { nb } from "date-fns/locale"
 
 export const humanizeDate = (date: Date) => {
   const now = new Date()
-
   const secondDifference = differenceInSeconds(date, now)
+
   if (secondDifference <= 0) {
     return "nå"
   }
 
-  const hourDifference = differenceInHours(date, now)
-  if (hourDifference < 1) {
-    return formatDistance(date, now, {
-      includeSeconds: true,
-      locale: nb,
-    }).replace("omtrent", "om")
+  if (secondDifference < 60) {
+    return `${secondDifference} sek`
+  }
+
+  if (differenceInMinutes(date, now) < 60) {
+    return `${Math.round(secondDifference / 60)} min`
   }
 
   if (isToday(date)) {
