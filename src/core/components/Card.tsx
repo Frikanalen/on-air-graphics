f8bb2bc26952421ba50c1535b71a89c15e0dca7c
Seb <enitoni@gmail.com>
2021-08-15 01:00:35 +0200

    Make card style reusable and tweak blur and shadow

--- src/core/components/Card.tsx
+++ src/core/components/Card.tsx
@@ -1,11 +1,15 @@
+import { css, Theme } from "@emotion/react"
 import styled from "@emotion/styled"
+
+export const CardStyle = (props: { theme: Theme }) => css`
+  background: ${props.theme.color.card};
+  box-shadow: 2px 2px 11px 2px rgba(0, 0, 0, 0.1);
+  backdrop-filter: blur(20px);
+`
 
 export const Card = styled.div`
   padding: 24px;
-
-  background: ${(props) => props.theme.color.card};
-  box-shadow: 2px 2px 11px 2px rgba(0, 0, 0, 0.05);
-
-  backdrop-filter: blur(16px);
   border-radius: 8px;
+
+  ${CardStyle}
 `
