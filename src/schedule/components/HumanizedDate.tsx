859dade50ce254ba61d111ee629176d171face9c
Seb <enitoni@gmail.com>
2021-08-14 13:52:28 +0200

    Add realtime humanized date and filter out expired schedule items

---
+++ src/schedule/components/HumanizedDate.tsx
@@ -0,0 +1,20 @@
+import React from "react"
+import { useEffect, useState } from "react"
+import { humanizeDate } from "../helpers/humanizeDate"
+
+export type HumanizedDateProps = {
+  date: Date
+}
+
+export function HumanizedDate(props: HumanizedDateProps) {
+  const { date } = props
+  const [humanized, setHumanized] = useState(humanizeDate(date))
+
+  useEffect(() => {
+    const interval = setInterval(() => setHumanized(humanizeDate(date)), 1000)
+
+    return () => clearInterval(interval)
+  }, [date])
+
+  return <>{humanized}</>
+}
