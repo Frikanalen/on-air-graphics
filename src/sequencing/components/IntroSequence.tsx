1f23585e9dc3a6e619ddb1aef76ace51cff88c4e
Seb <enitoni@gmail.com>
2021-08-15 21:40:49 +0200

    add IntroSequence component

---
+++ src/sequencing/components/IntroSequence.tsx
@@ -0,0 +1,105 @@
+import { css, keyframes } from "@emotion/react"
+import styled from "@emotion/styled"
+import React, { useEffect, useState } from "react"
+import { CardStyle } from "../../core/components/Card"
+import { Logo } from "../../core/components/Logo"
+import { wait } from "../../core/helpers/wait"
+
+export type AnimationState = "start" | "unblur" | "end"
+
+const Container = styled.div<{ state: AnimationState }>`
+  position: absolute;
+  top: 0px;
+  right: 0px;
+  bottom: 0px;
+  left: 0px;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  &:before {
+    content: "";
+    position: absolute;
+
+    height: 200%;
+    width: 200%;
+
+    top: 0px;
+    right: 0px;
+
+    transition: all 700ms ease-in-out;
+    transform: rotate(10deg) translateY(-200px);
+
+    ${(props) => {
+      if (props.state === "end")
+        return css`
+          transform: rotate(10deg) translateY(60%);
+        `
+
+      return
+    }}
+
+    ${CardStyle};
+  }
+`
+
+const LogoContainer = styled.div<{ state: AnimationState }>`
+  width: 600px;
+
+  position: relative;
+  z-index: 1;
+
+  transition: all 1000ms ease-in-out;
+
+  ${(props) => {
+    const { state } = props
+
+    if (state === "start")
+      return css`
+        filter: blur(120px);
+        opacity: 0;
+      `
+
+    if (state === "unblur") return
+
+    if (props.state === "end")
+      return css`
+        transition-duration: 700ms;
+
+        transform: translateY(800px);
+        opacity: 0;
+      `
+  }}
+`
+
+export type IntroSequenceProps = {
+  onFinished: () => void
+}
+
+export function IntroSequence(props: IntroSequenceProps) {
+  const [state, setState] = useState<AnimationState>("start")
+
+  useEffect(() => {
+    const run = async () => {
+      await wait(700)
+      setState("unblur")
+
+      await wait(2500)
+      setState("end")
+
+      await wait(700)
+      props.onFinished()
+    }
+
+    run()
+  }, [])
+
+  return (
+    <Container state={state}>
+      <LogoContainer state={state}>
+        <Logo />
+      </LogoContainer>
+    </Container>
+  )
+}
