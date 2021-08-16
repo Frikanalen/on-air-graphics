c71b4d6d9f6aef1b40183203f380ae8fce749e48
Seb <enitoni@gmail.com>
2021-08-16 02:11:46 +0200

    Add transition between screens

--- src/schedule/components/UpcomingView.tsx
+++ src/schedule/components/UpcomingView.tsx
@@ -1,12 +1,36 @@
+import { css } from "@emotion/react"
 import styled from "@emotion/styled"
 import React from "react"
 import { Card, CardStyle } from "../../core/components/Card"
 import { Clock } from "../../core/components/Clock"
 import { Logo } from "../../core/components/Logo"
 import { store } from "../../core/store"
+import {
+  TransitionState,
+  useTimedTransition,
+} from "../../sequencing/hooks/useTimedTransition"
 import { ScheduleItemSummary } from "./ScheduleItemSummary"
 
-const Container = styled.div`
+const slideFade = (px: number) => (props: { state: TransitionState }) => {
+  const { state } = props
+
+  if (state === "start" || state === "disappear")
+    return css`
+      opacity: 0;
+      transform: translateX(${px}px);
+    `
+}
+
+const slide = (px: number) => (props: { state: TransitionState }) => {
+  const { state } = props
+
+  if (state === "start" || state === "disappear")
+    return css`
+      transform: translateX(${px}px);
+    `
+}
+
+const Container = styled.div<{ state: TransitionState }>`
   display: flex;
   flex-direction: column;
 
@@ -22,6 +46,18 @@ const Container = styled.div`
     position: absolute;
 
     transform: rotate(10deg) translateY(-90px) translateX(70px);
+    transition: all 500ms ease;
+
+    ${(props) => {
+      const { state } = props
+
+      if (state === "start" || state === "disappear")
+        return css`
+          opacity: 0;
+          transform: rotate(10deg) translateY(-90px) translateX(200px);
+        `
+    }}
+
     ${CardStyle}
   }
 `
@@ -34,13 +70,24 @@ const Body = styled.div`
   z-index: 1;
 `
 
-const Footer = styled.div`
+const Footer = styled.div<{ state: TransitionState }>`
   text-align: center;
 
   font-weight: 600;
   font-size: 16px;
 
   color: ${(props) => props.theme.fontColor.muted};
+  transition: all 500ms ease;
+
+  ${(props) => {
+    const { state } = props
+
+    if (state === "start" || state === "disappear")
+      return css`
+        opacity: 0;
+        transform: translateY(200px);
+      `
+  }}
 
   position: relative;
   z-index: 1;
@@ -54,10 +101,17 @@ const Content = styled.div`
 const NextCard = styled(Card)`
   margin-top: 24px;
   margin-bottom: 42px;
+
+  transition: all 1000ms ease;
+  ${slide(-700)}
 `
 
 const LaterListCard = styled(Card)`
   margin-top: 24px;
+
+  transition: all 1000ms ease;
+  transition-delay: 100ms;
+  ${slide(-700)}
 `
 
 const Aside = styled.div`
@@ -70,50 +124,75 @@ const Aside = styled.div`
 
 const SizedLogo = styled(Logo)`
   width: 450px;
+
+  transition: all 500ms ease;
+  ${slideFade(50)}
 `
 
 const ClockContainer = styled.div`
   flex: 1;
   display: flex;
   align-items: center;
+
+  transition: all 500ms ease;
+  transition-delay: 100ms;
+  ${slideFade(50)}
 `
 
-export function UpcomingView() {
+const Heading = styled.h1<{ state: TransitionState }>`
+  transition: all 500ms ease;
+
+  ${(props) => {
+    const { state } = props
+
+    if (state === "start" || state === "disappear")
+      return css`
+        opacity: 0;
+      `
+  }}
+`
+
+export type UpcomingViewProps = {
+  duration: number
+  onFinished: () => void
+}
+
+export function UpcomingView(props: UpcomingViewProps) {
+  const { duration, onFinished } = props
   const [next, ...scheduleItems] = store.safeScheduleItems
 
-  const renderNext = () => {
-    if (!next) return null
+  const state = useTimedTransition({
+    duration,
+    appear: 1000,
+    disappear: 1000,
+    onFinished,
+  })
 
-    return (
-      <>
-        <h1>Neste program</h1>
-        <NextCard>
-          <ScheduleItemSummary item={next} />
-        </NextCard>
-      </>
-    )
-  }
+  console.log(state)
 
   return (
-    <Container>
+    <Container state={state}>
       <Body>
         <Content>
-          {renderNext()}
-          <h1>Senere</h1>
-          <LaterListCard>
+          <Heading state={state}>Neste program</Heading>
+          <NextCard state={state}>
+            {next ? <ScheduleItemSummary item={next} /> : null}
+          </NextCard>
+          <Heading state={state}>Senere</Heading>
+          <LaterListCard state={state}>
             {scheduleItems.slice(0, 3).map((item) => (
               <ScheduleItemSummary key={item.id} item={item} />
             ))}
           </LaterListCard>
         </Content>
         <Aside>
-          <SizedLogo />
-          <ClockContainer>
+          <SizedLogo state={state} />
+          <ClockContainer state={state}>
             <Clock size={320} />
           </ClockContainer>
         </Aside>
       </Body>
-      <Footer>
+      <Footer state={state}>
         Alt innhold sendes på medlemmers eget ansvar. Se frikanalen.no for
         kontakt- og redaktørinformasjon.
       </Footer>
--- src/sequencing/components/Screen.tsx
+++ src/sequencing/components/Screen.tsx
@@ -2,9 +2,11 @@ import React, { useEffect, useState } from "react"
 import styled from "@emotion/styled"
 
 import { Background } from "../../core/components/Background"
-import { Overlay } from "../../core/components/Overlay"
 
 import { RESOLUTION } from "../../core/constants"
+import { IntroSequence } from "./IntroSequence"
+import { wait } from "../../core/helpers/wait"
+import { UpcomingView } from "../../schedule/components/UpcomingView"
 const [width, height] = RESOLUTION
 
 const Container = styled.div`
@@ -29,17 +31,85 @@ const Content = styled.div<{ visible: boolean }>`
   opacity: ${(props) => (props.visible ? 1 : 0)};
 `
 
+const Overlay = styled.div<{ visible: boolean }>`
+  padding: 64px;
+
+  position: absolute;
+  top: 0px;
+  right: 0px;
+  bottom: 0px;
+  left: 0px;
+
+  &:before {
+    content: "";
+    background: ${(props) => props.theme.gradient.overlay};
+
+    position: absolute;
+    top: 0px;
+    right: 0px;
+    bottom: 0px;
+    left: 0px;
+
+    transition: opacity 500ms ease;
+    opacity: ${(props) => (props.visible ? 1 : 0)};
+  }
+`
+
+type SequenceItem = {
+  type: "upcoming"
+  duration: number
+}
+
 export function Screen() {
   const [visible, setVisible] = useState(false)
 
+  const [index, setIndex] = useState(0)
+  const [sequence, setSequence] = useState<SequenceItem[]>([])
+
+  const current = sequence[index]
+
+  const createSequence = () => {
+    // For testing purposes
+    setSequence([
+      { type: "upcoming", duration: 5000 },
+      { type: "upcoming", duration: 5000 },
+      { type: "upcoming", duration: 10000 },
+    ])
+  }
+
+  const advance = () => {
+    setIndex(index + 1)
+  }
+
   useEffect(() => {
-    setTimeout(() => setVisible(true), 500)
+    const run = async () => {
+      await wait(500)
+      setVisible(true)
+    }
+
+    run()
   }, [])
+
+  const renderContent = () => {
+    if (!current) return
+
+    if (current.type === "upcoming") {
+      return (
+        <UpcomingView
+          key={index}
+          duration={current.duration}
+          onFinished={advance}
+        />
+      )
+    }
+  }
 
   return (
     <Container>
       <Content visible={visible}>
         <Background />
+        <IntroSequence onFinished={createSequence} />
+        <Overlay visible={!!current}>{renderContent()}</Overlay>
       </Content>
     </Container>
   )
