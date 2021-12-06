6b571cdddf7d4c4266e1166708d42e2759a4cee4
Enitoni <enitoni@gmail.com>
2021-12-06 04:23:24 +0100

    Use webm instead

--- public/video/convert.sh
+++ public/video/convert.sh
@@ -1,6 +1,7 @@
 for i in ./mp4/*.mp4; do 
   name=`basename -- "$i" .mp4`
   echo "$name"
-  ffmpeg -i "$i" "./mkv/${name}.mkv"
+  ffmpeg  -i "$i"  -b:v 0  -crf 30  -pass 1  -an -f webm -y /dev/null
+  ffmpeg  -i "$i"  -b:v 0  -crf 30  -pass 2  "./webm/${name}.webm"
 done
 
