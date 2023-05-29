c6da9f3d995edbbe83dc3dd79e5246ada511f803
Tore Sinding Bekkedal <toresbe@protonmail.com>
2023-05-29 17:49:06 +0200

    refactor, works in casparcg again now

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,6 @@ import ReactDOM from "react-dom/client"
 import { App } from "./core/components/App"
 import "./index.css"
 
-console.log("main.tsx running")
-
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <App />
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/graphics"
+  base: "/graphics",
+  build: {
+    target: "es2019",
+  },
 })
