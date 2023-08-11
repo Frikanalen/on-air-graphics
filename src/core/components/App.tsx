2034efef96e4351ead5e4d9c34a4a609fbc4a6db
Tore Sinding Bekkedal <toresbe@protonmail.com>
2023-08-11 03:15:32 +0200

    Litt bedre DX

--- src/core/components/App.tsx
+++ src/core/components/App.tsx
@@ -1,13 +1,18 @@
 import { createContext, useState } from "react"
 import { Global, css, ThemeProvider, Theme } from "@emotion/react"
 import { useParams } from "../hooks/useParams"
-import { FADE_TRANSITION_MS, MINIMUM_SCREEN_TIME } from "../constants"
+import {
+  FADE_TRANSITION_MS,
+  MINIMUM_SCREEN_TIME,
+  RESOLUTION,
+} from "../constants"
 import { Content } from "./Content"
 import { getTheme } from "../../mood/helpers/getTheme"
 import { getPhaseOfDay } from "../../mood/helpers/getPhaseOfDay"
 import { OSLO_COORDINATES } from "../../mood/constants"
 import { useSchedule } from "../useSchedule"
 import { DevPanel } from "./DevPanel"
+import styled from "@emotion/styled"
 
 const globalStyle = (theme: Theme) => css`
   color: ${theme.fontColor.normal};
@@ -25,6 +30,26 @@ window.handleError = console.error
 window.handleWarning = console.log
 
 export type AppState = "idle" | "active" | "exit"
+
+const LoadingDiv = styled.div`
+  flex-grow: 1;
+  background: #333;
+  color: #ddd;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  min-height: 100vh;
+
+  div {
+    display: flex;
+    align-items: center;
+    flex-grow: 1;
+    font-size: 3em;
+    font-family: monospace;
+  }
+`
 
 export type AppContext = {
   /** Animation state */
@@ -59,16 +84,18 @@ export function App() {
     ),
   }
 
-  if (loading) return null
+  if (loading)
+    return (
+      <LoadingDiv>
+        <div>Loading...</div>
+      </LoadingDiv>
+    )
 
   return (
     <ThemeProvider theme={theme}>
       <Global styles={globalStyle} />
       <App.context.Provider value={context}>
-        <div className="bg-black w-screen h-screen flex justify-center items-center">
-          <Content />
-          <DevPanel />
-        </div>
+        {import.meta.env.DEV ? <DevPanel /> : <Content />}
       </App.context.Provider>
     </ThemeProvider>
   )
--- src/core/components/DevPanel.tsx
+++ src/core/components/DevPanel.tsx
@@ -1,23 +1,64 @@
 import styled from "@emotion/styled"
 import { RESOLUTION } from "../constants"
+import { Content } from "./Content"
+const DevContainerDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
 
-const DevPanelContainer = styled.div`
+  background: #333;
+  > div {
+    display: flex;
+    flex-direction: column;
+    max-width: ${RESOLUTION[0]}px;
+    gap: 2em;
+    h1 {
+      color: #999;
+      width: 100%;
+    }
+  }
+}`
+const DevPanelButtons = styled.div`
   background: black;
   width: ${RESOLUTION[0]}px;
   padding: 1em;
   display: flex;
-  align-items: center;
-  gap: 1em;
+  align-items: baseline;
+  gap: 2em;
   color: #ddd;
+
+  button {
+    background: #333;
+    color: #ddd;
+    font-weight: bold;
+    border: none;
+    padding: 0.5em 1em;
+    font-family: monospace;
+    cursor: pointer;
+    &:hover {
+      background: #444;
+    }
+    &:active {
+      background: #555;
+    }
+  }
 `
 export const DevPanel = () => {
   if (import.meta.env.PROD) return null
 
   return (
-    <DevPanelContainer>
-      <h2>Utviklermeny</h2>
-      <button onClick={window.play}>Play</button>
-      <button onClick={window.stop}>Stop</button>
-    </DevPanelContainer>
+    <DevContainerDiv>
+      <div>
+        <h1>Frikanalen sendegrafikk</h1>
+        <Content />
+        <DevPanelButtons>
+          <h2>Events</h2>
+          <button onClick={window.play}>PLAY</button>
+          <button onClick={window.stop}>STOP</button>
+        </DevPanelButtons>
+      </div>
+    </DevContainerDiv>
   )
 }
