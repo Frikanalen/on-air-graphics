9a645a68ec552d6af94d32786d6ebc3622b21827
Tore Sinding Bekkedal <toresbe@protonmail.com>
2024-02-22 17:17:40 +0100

    Log error on boot to check whether it makes it to casparcg logs

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom/client"
 import { App } from "./core/components/App"
 import "./index.css"
 
+console.error("Hello, world!")
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <App />
